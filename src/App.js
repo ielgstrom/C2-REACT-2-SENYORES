4d9f0a2d6493d2c1fedadbe0802073ee4d863fa0
Ignasi <i.elgstrom@gmail.com>
2021-06-11 13:13:17 +0200

    Añadido de datos complementarios

--- src/App.js
+++ src/App.js
@@ -1,3 +1,33 @@
-function App() {}
+function App() {
+  const senyores = [
+    {
+      id: 1,
+      nombre: "El Fary",
+      profesion: "Influencer",
+      foto: "fary.jpg",
+      estado: "R.I.P.",
+      twitter: "Pendiente",
+      marcado: true,
+    },
+    {
+      id: 2,
+      nombre: "Julio Iglesias",
+      profesion: "Youtuber",
+      foto: "julio.jpg",
+      estado: "Vivo",
+      twitter: "@JulyChurchs",
+      marcado: false,
+    },
+    {
+      id: 3,
+      nombre: "Bertín Osborne",
+      profesion: "Java Developer",
+      foto: "bertin.jpg",
+      estado: "Vivo",
+      twitter: "@BertinOsborne",
+      marcado: true,
+    },
+  ];
+}
 
 export default App;
